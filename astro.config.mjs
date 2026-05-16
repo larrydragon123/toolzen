33f6620d51111a90e24cbaf6595687e9634832b6
ToolZen <dev@toolzen.com>
2026-05-17 01:41:22 +0800

    chore: update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://toolzen-dqtah4coy-larrydragon.vercel.app',
+  site: 'https://toolzen-he0h9rbaf-larrydragon.vercel.app',
   output: 'static',
   trailingSlash: 'always'
 });
